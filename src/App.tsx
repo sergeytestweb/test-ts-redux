3b0b27bd6321175a91afc092bdc44287bd366626
Sergey <s.pozharitsky@intelico.su>
2023-07-14 16:22:05 +0300

    feat: final

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,87 @@
+// React App
+import { useState } from "react";
+// Material-UI Imports
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import Checkbox from "@material-ui/core/Checkbox";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from "@material-ui/icons/Delete";
+// Other Imports
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "./redux/store";
+import { addTodo, removeTodo, setTodoStatus } from "./redux/todoSlice";
+
 function App() {
+  //React Hooks
+  const [todoDescription, setTodoDescription] = useState("");
+
+  //React Redux Hooks
+  const todoList = useSelector((state: RootState) => state);
+  const dispatch = useDispatch<AppDispatch>();
+
+  //Rendering
   return (
-    <>
-      <h1>The test app ToDoList on typeScript && Redux for NovoSoft</h1>
-    </>
+    <Container maxWidth="xs">
+      <Typography style={{ textAlign: "center" }} variant="h3">
+        test Todo App for NovoSoft
+      </Typography>
+      <TextField
+        variant="outlined"
+        label="Write a new task here"
+        fullWidth
+        onChange={(e) => setTodoDescription(e.target.value)}
+        value={todoDescription}
+      />
+      <Button
+       variant="contained"
+       color="default"
+       fullWidth
+        onClick={() => {
+          dispatch(addTodo(todoDescription));
+          setTodoDescription("");
+        }}
+      >
+        Add Task
+      </Button>
+      <List>
+        {todoList.map((todo) => (
+          <ListItem key={todo.id}>
+            <ListItemText
+              style={{
+                textDecoration: todo.completed ? "line-through" : "none",
+              }}
+            >
+              {todo.description}
+            </ListItemText>
+            <ListItemSecondaryAction>
+              <IconButton
+                onClick={() => {
+                  dispatch(removeTodo(todo.id));
+                }}
+              >
+                <DeleteIcon />
+              </IconButton>
+              <Checkbox
+                edge="end"
+                color="default"
+                value={todo.completed}
+                onChange={() => {
+                  dispatch(
+                    setTodoStatus({ completed: !todo.completed, id: todo.id })
+                  );
+                }}
+              />
+            </ListItemSecondaryAction>
+          </ListItem>
+        ))}
+      </List>
+    </Container>
   );
 }
 
